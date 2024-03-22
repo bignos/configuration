6c100401308502a83c043cd2540f1fbef377f717
Balandjian <j.balandjian@criiamsud.fr>
2024-03-22 16:30:29 +0100

    New lsp-zero configuration with dap

--- neovim_lua/lua/plugins/lsp-zero.lua
+++ neovim_lua/lua/plugins/lsp-zero.lua
@@ -1,12 +1,15 @@
 return {
     {
         'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
+        branch = 'v3.x',
         dependencies = {
             -- LSP Support
             { 'neovim/nvim-lspconfig' }, -- Required
             { 'williamboman/mason.nvim' }, -- Optional
             { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+            { 'mfussenegger/nvim-dap' }, -- Optional
+            { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} },
+            { 'jay-babu/mason-nvim-dap.nvim' }, -- Optional
 
             -- Autocompletion
             { 'hrsh7th/nvim-cmp' }, -- Required
@@ -28,28 +31,72 @@ return {
                 suggest_lsp_servers = false,
             })
 
-            -- (Optional) Configure lua language server for neovim
-            lsp.nvim_workspace()
+            require('mason').setup()
+            require('mason-nvim-dap').setup({
+                ensure_installed = { 'python', 'stylua' },
+                handlers = {},
+            })
+            require('mason-lspconfig').setup({
+                ensure_installed = {},
+                handlers = {
+                    lsp.default_setup,
+                    lua_ls = function()
+                        local lua_opts = lsp.nvim_lua_ls()
+                        require('lspconfig').lua_ls.setup(lua_opts)
+                    end,
+                }
+            })
+            require('dapui').setup()
+
+            lsp.set_sign_icons({
+              error = '✘',
+              warn = '▲',
+              hint = '⚑',
+              info = ''
+            })
+
+            vim.diagnostic.config({
+              virtual_text = false,
+              severity_sort = true,
+              float = {
+                style = 'minimal',
+                border = 'rounded',
+                source = 'always',
+                header = '',
+                prefix = '',
+              },
+            })
 
             local cmp = require('cmp')
+            local cmp_action = lsp.cmp_action()
+            local cmp_format = lsp.cmp_format()
+
             cmp.setup({
+                formatting = cmp_format,
+                preselect = 'item',
                 completion = {
                     autocomplete = false,  -- No toggle auto the completion menu
                 },
+                sources = {
+                    {name = 'path'},
+                    {name = 'nvim_lsp'},
+                    {name = 'nvim_lua'},
+                    {name = 'buffer', keyword_length = 3},
+                    {name = 'luasnip', keyword_length = 2},
+                },
+                window = {
+                    documentation = cmp.config.window.bordered(),
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                    ['<cr>'] = cmp.mapping.confirm({ select = true }),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                })
             })
 
             local cmp_select = { behavior = cmp.SelectBehavior.Select }
-            local cmp_mappings = lsp.defaults.cmp_mappings({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ['<cr>'] = cmp.mapping.confirm({ select = true }),
-                ['<C-Space>'] = cmp.mapping.complete(),
-            })
-
-            lsp.setup_nvim_cmp({
-                mapping = cmp_mappings
-            })
 
             lsp.on_attach(function(_, bufnr)
                 local opts = { buffer = bufnr, remap = false }
@@ -65,6 +112,16 @@ return {
                 vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
                 vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
                 vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+
+                -- DAP
+                vim.keymap.set('n', '<leader>Db', function() require('dap').toggle_breakpoint() end, {desc = "Toggle Breakpoint"})
+                vim.keymap.set('n', '<leader>Dc', function() require('dap').continue() end, {desc = "Continue"})
+                vim.keymap.set('n', '<leader>Di', function() require('dap').step_into() end, {desc = "Step Into"})
+                vim.keymap.set('n', '<leader>Do', function() require('dap').step_over() end, {desc = "Step Over"})
+                vim.keymap.set('n', '<leader>DO', function() require('dap').step_out() end, {desc = "Step Out"})
+                vim.keymap.set('n', '<leader>Dr', function() require('dap').repl.open() end, {desc = "Repl"})
+                vim.keymap.set('n', '<leader>Dl', function() require('dap').run_last() end, {desc = "Run Last"})
+                vim.keymap.set('n', '<leader>Dt', function() require('dapui').toggle() end, {desc = "Toggle UI"})
             end)
 
             lsp.setup()
